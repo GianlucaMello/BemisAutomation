e6176721cda96f28fdb795e3bbec1f340407b17d
gianlucamello <gianlucademello@Gianlucas-MacBook-Pro.local>
2022-01-20 11:46:22 -0300

    Setup project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # BemisAutomation
 Automation for the Bemis project with Codecept JS and Appium.
+
+How to install   
+    npm install
+
+How to run
+    terminal 1
+    appium
+
+    terminal 2
+    npx codeceptjs run --steps
---
+++ codecept.conf.js
@@ -0,0 +1,34 @@
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Appium: {
+      platform: 'Android',
+      app: '/Users/gianlucademello/Desktop/Bemis_image/Bemis.apk',
+      desiredCapabilities: {
+        deviceName: 'Bemis_android',
+        platformVersion: '12',
+        appPackage: '',
+        appActivity: ''
+      }
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'BemisAutomation',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
